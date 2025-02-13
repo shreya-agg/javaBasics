449bb76e5931d0f774c984300d1c932282886088
Shreya Agarwal <shreya.agarwal.sa@gmail.com>
2025-02-14 04:51:25 +0530

    java files

---
+++ OddEvenNumber.java
@@ -0,0 +1,18 @@
+package numbers;
+
+import java.util.Scanner;
+
+public class OddEvenNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+        scanner.close();
+    }
+}
+
